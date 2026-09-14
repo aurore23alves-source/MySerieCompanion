da0e57776a18f4efcefcd7b9f8298b6cb3cf95cb
Aurore Alves <aurore23.alves@gmail.com>
2026-09-14 13:53:28 +0200

    commentaire ajout série

--- MySeriesCompanion/MySeriesCompanion/public/ajouter-serie.php
+++ MySeriesCompanion/MySeriesCompanion/public/ajouter-serie.php
@@ -1,16 +1,20 @@
-
 <?php
+// Connexion à la base de données  et fonctions
 require_once "../config/database.php";
 require_once "../includes/functions.php";
 
 $titrePage = "Ajouter une série";
 $erreurs = [];
 
+// Vérification du formulaire
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    // Données du formulaire
     $nom = trim($_POST["nom"] ?? "");
     $resume = trim($_POST["resume"] ?? "");
     $dateSortie = $_POST["date_sortie"] ?? "";
 
+    // Champs obligatoires
     if ($nom === "") {
         $erreurs[] = "Le nom de la série est obligatoire.";
     }
@@ -19,15 +23,17 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $erreurs[] = "La date de sortie est obligatoire et doit être valide.";
     }
 
-     $vignette = "";
+    // Vignette
+    $vignette = "";
     if (empty($erreurs) && isset($_FILES["vignette"])) {
         $vignette = enregistrerImage($_FILES["vignette"]);
 
         if ($_FILES["vignette"]["error"] !== UPLOAD_ERR_NO_FILE && $vignette === "") {
             $erreurs[] = "La vignette doit être une image JPG, JPEG, PNG ou WEBP.";
-         }
+        }
     }
 
+    // Si aucune erreur, on enregistre la série dans la base de données
     if (empty($erreurs)) {
         $sql = "INSERT INTO serie (nom, resume, vignette, date_sortie)
                 VALUES (:nom, :resume, :vignette, :date_sortie)";
@@ -40,11 +46,12 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             "date_sortie" => $dateSortie
         ]);
 
+        // Récupère l'ID de la série créée
         $idSerie = $pdo->lastInsertId();
 
-         // Après l'ajout, on va directement sur le détail de la série.
+        // Redirige vers la page de la série
         header("Location: serie.php?id=" . $idSerie);
-         exit;
+        exit;
     }
 }
 
@@ -57,13 +64,16 @@ require "../includes/header.php";
 </section>
 
 <?php if (!empty($erreurs)): ?>
+    
     <div class="error">
         <?php foreach ($erreurs as $erreur): ?>
             <p><?= e($erreur) ?></p>
-         <?php endforeach; ?>
+        <?php endforeach; ?>
     </div>
 <?php endif; ?>
 
+<!-- Formulaire d'ajout d'une série -->
+ 
 <form class="form-card" method="POST" enctype="multipart/form-data">
 
     <div class="form-group">
